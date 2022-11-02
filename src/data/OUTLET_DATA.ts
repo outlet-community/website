cbc98feb9dad87c9139e52b123313300db390f7d
Domenik Töfflinger <domenikt96@yahoo.de>
2022-11-02 19:25:35 +0100

    feat: create reusable OUTLET_DATA.ts

---
+++ src/data/OUTLET_DATA.ts
@@ -0,0 +1,15 @@
+const OUTLET_DATA = {
+  mail: 'community@outlet.delivery',
+  orgName: 'Outlet Community Platform e.V.',
+  shortName: 'Outlet',
+  radio: {
+    name: 'Outlet Radio',
+    mail: 'radio@outlet.delivery',
+  },
+  label: {
+    name: 'Outlet Label',
+    mail: 'label@outlet.delivery',
+  },
+}
+
+export default OUTLET_DATA
